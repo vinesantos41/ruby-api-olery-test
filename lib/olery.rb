a3df16fe8fa53446fa2acc61b7fb27bf49ab8346
Vinicius <vinesantos41@gmail.com>
2022-08-07 11:56:44 -0300

    Require new files

--- lib/olery.rb
+++ lib/olery.rb
@@ -9,10 +9,15 @@ require 'sequel'
 #Controllers
 require_relative 'olery/controllers/base'
 require_relative 'olery/controllers/v1/users'
+require_relative 'olery/controllers/v1/user_hotels'
 
 #Models
 require_relative 'olery/models/user'
 require_relative 'olery/models/contract'
+require_relative 'olery/models/group_subscription'
+require_relative 'olery/models/group'
+require_relative 'olery/models/hotel'
+require_relative 'olery/models/hotel_group'
 
 #Server
 require_relative 'olery/v1_server'
